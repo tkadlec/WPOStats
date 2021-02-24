25544b7620d383c2f6a9ef41eba5e3502cde20fb
Tim Kadlec <tim@timkadlec.com>
2021-02-24 17:07:06 -0600

    clean up speed test function

--- functions/get-speedtest-url.js
+++ functions/get-speedtest-url.js
@@ -3,30 +3,16 @@ const NetlifyAPI = require('netlify');
 
 exports.handler = async function(event, context) {
     // get latest testId
-
     const client = new NetlifyAPI(ACCESS_TOKEN);
-    console.log("client is in....");
     //fetch forms
-    try {
-        const forms = await client.listFormSubmissions({
-            formId: '6036cdd27d632a0007c2691f'
-        })
-        console.log(forms[0].data.testURL);
-        return {
-            statusCode: '302',
-            headers: {
-                Location: forms[0].data.testURL
-            }
+    const forms = await client.listFormSubmissions({
+        formId: '6036cdd27d632a0007c2691f'
+    })
+    return {
+        statusCode: '302',
+        headers: {
+            Location: forms[0].data.testURL
         }
-    } catch (error) {
-        console.log("error....");
-        console.log(error);
-        return { statusCode: 422, body: error};
     }
-    
-    // //serve redirect
-    // return {
-    //     statusCode: 302,
-    //     body: JSON
-    // }
+
 }
