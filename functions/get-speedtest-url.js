a9e13ea809ef936c5f8325c9ea5986f9b125b6ec
Tim Kadlec <tim@timkadlec.com>
2021-02-24 16:51:48 -0600

    back to netlify api

--- functions/get-speedtest-url.js
+++ functions/get-speedtest-url.js
@@ -1,23 +1,27 @@
 const { ACCESS_TOKEN, SITE_ID } = process.env;
-const fetch = require('fetch');
+const NetlifyAPI = require('netlify');
 
 exports.handler = async function(event, context) {
     // get latest testId
 
-    fetch(`https://api.netlify.com/api/v1/sites/${SITE_ID}/forms`, {
-        headers: {
-            "Authorization": "Bearer " + ACCESS_TOKEN
-        }
-    }).then((response) => {
-        response.json().then((json) => {
-            console.log("response....");
-            console.log(json);
-            return {
-                statusCode: '200',
-                body: JSON.stringify(json)
-            }
+    const client = new NetlifyAPI(ACCESS_TOKEN);
+    console.log("client is in....");
+    //fetch forms
+    try {
+        const forms = await client.listSiteForms({
+            siteId: SITE_ID
         })
-    })
+        console.log("forms....");
+        console.log(forms);
+        return {
+            statusCode: '200',
+            body: JSON.stringify(forms)
+        }
+    } catch (error) {
+        console.log("error....");
+        console.log(error);
+        return { statusCode: 422, body: "RUH ROH"};
+    }
     
     // //serve redirect
     // return {
