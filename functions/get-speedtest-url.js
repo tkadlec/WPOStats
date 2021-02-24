984b23f1b047c0ba959580f8adb4fe992cb5e061
Tim Kadlec <tim@timkadlec.com>
2021-02-24 16:35:33 -0600

    debug logs to function

--- functions/get-speedtest-url.js
+++ functions/get-speedtest-url.js
@@ -4,17 +4,21 @@ const NetlifyAPI = require('netlify');
 exports.handler = async function(event, context) {
     // get latest testId
     const client = new NetlifyAPI(ACCESS_TOKEN);
-
+    console.log("client is in....");
     //fetch forms
     try {
         const forms = await client.listSiteForms({
             siteId: SITE_ID
         })
+        console.log("forms....");
+        console.log(forms);
         return {
             statusCode: '200',
             body: JSON.stringify(forms)
         }
     } catch (error) {
+        console.log("error....");
+        console.log(error);
         return { statusCode: 422, body: "RUH ROH"};
     }
     
